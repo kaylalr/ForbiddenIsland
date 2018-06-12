10c9646286cc1e7e0460f4c15072a111d38ae84e
Kayla <Kayla@192.168.52.131>
2018-06-12 13:30:37 -0600

    6-12-18 Finished most of the Game Menu

---
+++ forbiddenIsland/src/citbyui/cit260/forbiddenisland/view/CaptureTreasureView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.forbiddenisland.view;
+
+/**
+ *
+ * @author DD
+ */
+class CaptureTreasureView {
+
+    void displayCaptureTreasureView() {
+        System.out.println("*** displayCaptureTreasureView() called ***");
+    }
+    
+}
