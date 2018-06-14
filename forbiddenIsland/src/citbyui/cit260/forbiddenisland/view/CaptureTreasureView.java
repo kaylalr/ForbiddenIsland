ff5b0eba374565c44abfa8710b718441fc5f1874
mcwiseman97 <mcwiseman97@gmail.com>
2018-06-14 13:29:52 -0600

    6-14-2018 Added content to the CatureTreasureView class, gave the UnfloodTileView an If-Else statement

--- forbiddenIsland/src/citbyui/cit260/forbiddenisland/view/CaptureTreasureView.java
+++ forbiddenIsland/src/citbyui/cit260/forbiddenisland/view/CaptureTreasureView.java
@@ -37,28 +37,38 @@ class CaptureTreasureView {
             Scanner inFile;
             inFile = new Scanner(System.in);
 
+            System.out.println("How many trophies are left: ");
             System.out.println("Please enter Y or N: ");
             System.out.println("Y - Yes: ");
             System.out.println("N - No: ");
-            String value = inFile.nextLine().trim();
+            System.out.println("Q - return to previous menu");
+            String value = inFile.nextLine().trim().toUpperCase();
 
             if (value.length() < 1 || value.length() > 1) {
                 System.out.println("Error: Please enter either Y or N: ");
                 continue;
             }
 
-
             inputs[0] = value;
             valid = true;
         }
-
         return inputs;
     }
 
     private boolean doActions(String[] inputs) {
-        System.out.println("You have selected - " + inputs[0]);
-
+        switch (inputs[0]){
+            case "Y":
+                System.out.println("Congrats! You have collected the _______ trophy!");
+                break;
+            case "N":
+                System.out.println("You have decided to leave the trophy...");
+                break;
+//            case "Q":
+//                break;
+            default:
+                System.out.println("Error: Try again...");
+                return false;
+        }
         return true;
     }
-
 }
