779437c5932b90e3f3802c089816cd394cb32090
mcwiseman97 <mcwiseman97@gmail.com>
2018-06-14 11:43:20 -0600

    6-14-2018 Added content to the UnfloodTileView class

--- forbiddenIsland/src/citbyui/cit260/forbiddenisland/view/UnfloodTileView.java
+++ forbiddenIsland/src/citbyui/cit260/forbiddenisland/view/UnfloodTileView.java
@@ -5,6 +5,8 @@
  */
 package citbyui.cit260.forbiddenisland.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author DD
@@ -12,7 +14,48 @@ package citbyui.cit260.forbiddenisland.view;
 class UnfloodTileView {
 
     void displayUnfloodTileView() {
-        System.out.println("*** displayUnfloodTileView() ***");
+        boolean endOfView = false;
+
+        do {
+            String[] inputs = this.getInputs();
+            if (inputs == null || inputs[0].toUpperCase().equals("Q")) {
+                return;
+            }
+
+            endOfView = doActions(inputs);
+
+        } while (endOfView != true);
+    }
+    
+        private String[] getInputs() {
+        String[] inputs = new String[1];
+        System.out.println("Which tile would you like to unflood? ");
+ 
+        boolean valid  = false;
+        while (valid == false){
+            
+            Scanner inFile;
+            inFile = new Scanner(System.in);
+            
+            System.out.println("Please enter coordinates: ");
+            String value = inFile.nextLine().trim();
+            
+            if(value.length() < 2 || value.length() > 3){
+                System.out.println("Error: Enter a valid coordinate: ");
+                continue;
+            }
+            
+            inputs[0] = value;
+            valid = true;
+        }
+        
+        return inputs;
+    }
+
+    private boolean doActions(String[] inputs) {
+        System.out.println("You have selected - " + inputs[0]);
+ 
+        return true;
     }
     
 }
