a7e043e22d5a68a735e866a207e5d1731a7f9569
Kayla <Kayla@192.168.52.131>
2018-07-19 14:46:29 -0600

    7-19-18 worked on capturing treasure

--- forbiddenIsland/src/citbyui/cit260/forbiddenisland/view/CaptureTreasureView.java
+++ forbiddenIsland/src/citbyui/cit260/forbiddenisland/view/CaptureTreasureView.java
@@ -5,6 +5,10 @@
  */
 package citbyui.cit260.forbiddenisland.view;
 
+import citbyui.cit260.model.Actor;
+import citbyui.cit260.model.Location;
+import forbiddenisland.ForbiddenIsland;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -17,12 +21,30 @@ public class CaptureTreasureView extends View {
     public String[] getInputs() {
         String[] inputs = new String[1];
 
-        this.console.println("Would you like to capture the ________ treasure?\n"
-                + "Trophies left: #\n"
-                + "Y - Yes\n"
-                + "N - No\n");
+        Location[][] locations = ForbiddenIsland.getCurrentGame().getMap().getLocation();
+        ArrayList<Actor> currActors = ForbiddenIsland.getPlayer().getActors();
+        Actor actor = currActors.get(0);
+        int row = actor.getCoordinates().x;
+        int col = actor.getCoordinates().y;
+        String treasureName = locations[row][col].getTreasure().getName();
+        if (!locations[row][col].getTreasure().getName().equals("Empty")) {
+            if (locations[row][col].getTreasure().getStatus().equals("Captured")) {
+                this.console.println("You have already captured the " + treasureName + " treasure.\n");
+                inputs[0] = "Q";
+            } else {
+                this.console.println("Would you like to capture the " + treasureName + " treasure?\n"
+                        //                    + "Treasures left: #\n"
+                        + "Y - Yes\n"
+                        + "N - No\n");
+                inputs[0] = this.getInput("Please enter Y or N:");
+            }
+        } else {
+            this.console.println("\n**********************************************\n"
+                    + "       There is not a treasure here.\n"
+                    + "**********************************************\n");
+            inputs[0] = "Q";
+        }
 
-        inputs[0] = this.getInput("Please enter Y or N:");
         return inputs;
     }
 
@@ -32,15 +54,22 @@ public class CaptureTreasureView extends View {
             ErrorView.display(this.getClass().getName(), "Error: Invalid value.");
             return false;
         }
+        Location[][] locations = ForbiddenIsland.getCurrentGame().getMap().getLocation();
+        ArrayList<Actor> currActors = ForbiddenIsland.getPlayer().getActors();
+        Actor actor = currActors.get(0);
+        int row = actor.getCoordinates().x;
+        int col = actor.getCoordinates().y;
+        String treasureName = locations[row][col].getTreasure().getName();
         switch (inputs[0]) {
             case "Y":
-                this.console.println("Congrats! You have collected the _______ trophy!");
+                locations[row][col].getTreasure().setStatus("Captured");
+                this.console.println("Congrats! You have collected the " + treasureName + " trophy!");
                 break;
             case "N":
                 this.console.println("You have decided to leave the trophy...");
                 break;
-//            case "Q":
-//                break;
+            case "Q":
+                break;
             default:
                 ErrorView.display(this.getClass().getName(), "Error: Try again...");
                 return false;
